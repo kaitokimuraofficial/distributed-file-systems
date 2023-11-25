4e7681002c33d14e6963dda5a60665afe95437e0
kaitokimuraofficial <k.kaito0422@akane.waseda.jp>
2023-11-25 14:05:42 +0900

    Update FileTest

--- test/File/FileTest.java
+++ test/File/FileTest.java
@@ -1,6 +1,10 @@
 package test.File;
 
+import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
+import java.nio.charset.StandardCharsets;
+
+import org.junit.Before;
 import org.junit.Test;
 
 import src.File.File;
@@ -11,30 +15,82 @@ import src.File.File;
 */
 
 public class FileTest {
-    File file1 = new File.Builder(1, "test").
-    isReadAllowed(true).isWriteAllowed(true).build();
+    private File file1;
 
-    @Test
-    public void isReadAllowedIsTrue() {
-        assertEquals(true, file1.getIsReadAllowed());
+    @Before
+    public void setup() {
+        file1 = new File.Builder(1, "test").
+        isReadAllowed(true).isWriteAllowed(true).build();
+        file1.setFileContent("FUCK");
     }
 
     @Test
-    public void isWriteAllowedIsTrue() {
+    public void testGetCreatedBy() {
+        assertEquals(1, file1.getCreatedBy());
+    }
+
+    @Test
+    public void testGetFileName() {
+        assertEquals("test", file1.getFileName());
+    }
+    
+    @Test
+    public void testGetIsReadAllowed() {
+        assertEquals(true, file1.getIsReadAllowed());
+    }
+    
+    @Test
+    public void testGetIsWriteAllowed() {
         assertEquals(true, file1.getIsWriteAllowed());
     }
 
     @Test
-    // 書き込まれるテキストが設定されたデフォルトサイズのバイト配列に収まる場合のテスト
-    public void testWriteTextWithinLimit() {
-        String testString = "FUCK";
-        file1.write(testString);
-        assertEquals(4, file1.write(testString));
+    public void testGetLastModiiedBy() {
+        assertEquals(1, file1.getCreatedBy());
+    }
+    
+    @Test
+    public void testSetFileContent() {
+        assertEquals(4, file1.setFileContent("FUCK"));
     }
 
     @Test
-    // テキストを読む
-    public void testReadTextWithinLimit() {
-        assertEquals(11, file1.read());
+    public void testGetFileContentWhenLengthIsWithinLimit() {
+        String text = "SHIT";
+        file1.setFileContent(text);
+        assertArrayEquals(text.getBytes(StandardCharsets.US_ASCII), file1.getFileContent());
+    }
+
+    @Test
+    public void testGetFileContentWhenLengthIsZero() {
+        String text = "";
+        file1.setFileContent(text);
+        assertArrayEquals(text.getBytes(StandardCharsets.US_ASCII), file1.getFileContent());
+    }
+
+    @Test
+    public void testGetFileContentWhenLengthIsOverDEFAULTSIZE() {
+        String text = "FUCK".repeat(4096);
+        file1.setFileContent(text);
+        String textExpected = "FUCK".repeat(1024);
+        assertArrayEquals(textExpected.getBytes(StandardCharsets.US_ASCII), file1.getFileContent());
+    }
+
+    @Test
+    public void testSetFileName() {
+        file1.setFileName("FUCK");
+        assertEquals("FUCK", file1.getFileName());
+    }
+
+    @Test
+    public void testSetIsReadAllowed() {
+        file1.setIsReadAllowed(false);
+        assertEquals(false, file1.getIsReadAllowed());
+    }
+
+    @Test
+    public void testSetIsWriteAllowed() {
+        file1.setIsWriteAllowed(false);
+        assertEquals(false, file1.getIsWriteAllowed());
     }
 }
