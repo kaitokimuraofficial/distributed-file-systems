d4d2b2df86af3661d738ef30a69ecad18f91ca20
serpent <pac.ttk0917@gmail.com>
2024-01-15 18:45:22 +0900

    feat: FileContainerクラスの実装

---
+++ src/file/FileContainer.java
@@ -0,0 +1,25 @@
+package src.file;
+
+import java.io.File;
+
+/**
+* ファイル
+* @author Keisuke Nakao
+*/
+
+public class FileContainer extends File {
+    private byte[] fileContent;
+
+    public FileContainer(String pathName, byte[] fileContent) {
+        super(pathName);
+        this.fileContent = fileContent;
+    }
+
+    public byte[] getFileContent() {
+        return this.fileContent;
+    }
+
+    public void setFileContent(byte[] fileContent) {
+        this.fileContent = fileContent;
+    }
+}
