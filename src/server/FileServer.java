ffd886b3ff03769e354af556ed3e53a785f034fd
3kanAlpha <39447635+3kanAlpha@users.noreply.github.com>
2024-01-15 16:22:09 +0900

    feat: FileServerを簡易的に実装

--- src/server/FileServer.java
+++ src/server/FileServer.java
@@ -1,14 +1,64 @@
 package src.server;
 
-/** 
-* ファイルサーバー
-* @author　Kaito Kimura
-*/
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+/**
+ * ファイルサーバー
+ * @author Kaito Kimura
+ * @author Aoyagi Tomoya
+ */
 
 public class FileServer {
-    int number = 1;
+    /** このパスをルートとしてファイルサーバーを立てる */
+    private final Path root;
 
-    public void start(){
-        System.out.println("number");
+    public FileServer(Path root) {
+        this.root = root;
     }
+
+    /**
+     * 与えられたパスから、実際のファイルサーバー上のパスを取得する
+     * @param filePath 取得したいファイルのパス
+     * @return ファイルサーバー上のファイルのパス
+     */
+    private Path getAbsolutePath(Path filePath) {
+        return this.root.resolve(filePath).toAbsolutePath();
+    }
+
+    /**
+     * 与えられたパスのファイルの内容を読み込み、バイト列として返す
+     * @param filePath 読み込みたいファイルのパス
+     * @return ファイルの内容
+     */
+    public byte[] readFile(Path filePath) {
+        Path p = getAbsolutePath(filePath);
+
+        try {
+            return Files.readAllBytes(p);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    /**
+     * 与えられたパスのファイルに、バイト列を書き込む
+     * @param filePath 書き込みたいファイルのパス
+     * @param data 書き込む内容
+     * @return 書き込みに成功したかどうか
+     */
+    public boolean writeFile(Path filePath, byte[] data) {
+        Path p = getAbsolutePath(filePath);
+
+        try {
+            Files.write(p, data);
+            return true;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
 }
